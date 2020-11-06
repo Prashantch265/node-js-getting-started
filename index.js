f4e0c4e4064a1d6f80b343e104dd5c9f49cc1070
Prashantch265 <prashantchy265@gmail.com>
2020-11-06 15:56:25 +0545

    try-1 fix error provision db

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-const {Pool} = require('pg')
-const Pool = new Pool({
+const { Pool } = require('pg');
+const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl:{
+  ssl: {
     rejectUnauthorized: false
   }
 });
